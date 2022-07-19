b7de6e7c5286e8ae8be5cd61249ebf67050d6b43
vaishali023 <vaishalichaudhary011@gmail.com>
2022-07-19 15:22:28 -0400

    feat: Style navigation and logo

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -3,13 +3,18 @@ import {Routes, Route} from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 
+const Shop = () => {
+  <div >Hello</div>
+}
+
 const App = () => {
   
     return (
       <Routes>
-      <Route path='/' element = {<Navigation/>}></Route>
-      <Route path='/' element={ <Home />}/>
-       
+      <Route path='/' element = {<Navigation/>}>
+        <Route index element={ <Home />}/>
+        <Route path='shop' element= {<Shop />} />
+       </Route>
       </Routes>
     );
 };
--- crwn-clothing/src/routes/navigation/navigation.component.jsx
+++ crwn-clothing/src/routes/navigation/navigation.component.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Fragment } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+
+import './navigation.styles.scss';
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className='navigation'>
-         <h1>I am the Navigation Bar.</h1>
+      <Link className='logo-container' to='/'>
+      <CrwnLogo className='logo' />
+      </Link>
+        <div className='nav-links-container'>
+        <Link className='nav-link' to='/shop'>
+           SHOP
+        </Link>
+
+        </div>
       </div>
       <Outlet />
     </Fragment>
