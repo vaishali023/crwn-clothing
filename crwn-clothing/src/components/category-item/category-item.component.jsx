34ece93acfceb6f78b09043ec324437df354b9ee
vaishali023 <vaishalichaudhary011@gmail.com>
2022-08-18 21:42:15 -0400

    Refractor: Update Categories

--- crwn-clothing/src/components/category-item/category-item.component.jsx
+++ crwn-clothing/src/components/category-item/category-item.component.jsx
@@ -3,10 +3,11 @@ import "./category-item.styles.scss";
 const CategoryItem = ({category}) => {
     const {imageUrl, title} = category;
   return (
-
-    <div key={id} className="category-container">
-    <div className="background-image" style={{
-      backgroundImage : `url(${imageUrl})`
+    <div className="category-container">
+    <div
+     className="background-image"
+      style={{
+      backgroundImage : `url(${imageUrl})`,
     }} />
     <div className="category-body-container">
       <h2>{title}</h2>
