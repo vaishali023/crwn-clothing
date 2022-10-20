5015f8464810e6700bd9877e484ab8c673b27047
vaishali023 <vaishalichaudhary011@gmail.com>
2022-10-19 22:57:19 -0400

    Refractor: Product card component

--- crwn-clothing/src/routes/shop/shop.component.jsx
+++ crwn-clothing/src/routes/shop/shop.component.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
-import { ProductsContext } from "../../context/products.context";
 
+import { ProductsContext } from "../../context/products.context";
+import ProductCard from "../../components/product-card/product-card.component";
 
 const Shop = () => {
     const {products} = useContext(ProductsContext);
