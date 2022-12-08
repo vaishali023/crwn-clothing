f7b74abdcd220c79e06b4de4c301335e628fc70a
vaishali023 <vaishalichaudhary011@gmail.com>
2022-12-07 21:49:06 -0500

    Feat: Add Redux

--- crwn-clothing/src/contexts/user.context.jsx
+++ crwn-clothing/src/contexts/user.context.jsx
@@ -1,4 +1,5 @@
-import {createContext, useState, useEffect} from 'react';
+import {createContext, useEffect, useReducer} from 'react';
+import { createAction } from '../utils/reducer/reducer.utils';
 
 import { createUserDocumentFromAuth,
      onAuthStateChangedListener} from '../utils/firebase/firebase.utils';
@@ -8,19 +9,40 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+
+const INITIAL_STATE = {
+    currentUser: null,
+};
+
+const userReducer = (state, action) => {
+    const { type, payload} = action;
+
+  switch(type) {
+    case 'SET_CURRENT_USER': 
+    return {
+        ...state,
+        currentUser: payload
+    }
+    
+    default: 
+    throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+
+
+
 export const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
+    const [ {currentUser}, dispatch] = useReducer(userReducer, INITIAL_STATE);
+    
+    const setCurrentUser = (user) => {
+        dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));  
+    }
+
     const value = { currentUser, setCurrentUser};
 
-    useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-        if(user) {
-            createUserDocumentFromAuth(user);
-        }
-        setCurrentUser(user);
-    } );
-
-    return unsubscribe;
-    }, []);
+   
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 };
--- crwn-clothing/src/index.js
+++ crwn-clothing/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import App from './App';
 import { UserProvider } from './contexts/user.context';
 import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
+import { store } from './store/store';
 
 import './index.scss';
 
@@ -14,15 +16,15 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <Provider store={store}>
    <BrowserRouter>
-      <UserProvider>
        <CategoriesProvider>
           <CartProvider>
            <App />
           </CartProvider>
        </CategoriesProvider>
-      </UserProvider>
    </BrowserRouter>
+   </Provider>
   </React.StrictMode>
 );
 
